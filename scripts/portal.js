1af9fd2c2cbfa61d805ca134dc8d05c7ac2e652c
Devine Lu Linvega <aliceffekt@gmail.com>
2018-06-03 10:11:00 +1200

    Minor changes to the layout

--- scripts/portal.js
+++ scripts/portal.js
@@ -1,6 +1,6 @@
 function Portal(sites)
 {
-  this.el = document.createElement("pre");
+  this.el = document.createElement("div");
   this.sites = sites;
   
   this.install = function()
@@ -13,6 +13,16 @@ function Portal(sites)
     this.install();
     this.el.innerHTML = window.location.hash && window.location.hash.length > 4 ? this.redirect() : this.directory();
   }
+
+  this.readme = function()
+  {
+    return `<p class='readme'>This webring is an attempt to inspire artists & developers to build their own website and share traffic among each other. The ring welcomes personalized websites such as <b>diaries, wikis & portfolios</b> — To add yourself to the ring, submit a <a href='https://github.com/XXIIVV/webring/edit/master/index.html' target='_blank'>Pull Request</a>.</p>`
+  }
+
+  this.buttons = function()
+  {
+    return `<p class='buttons'><a href='#random' onClick="portal.reload('random')">Random</a> | <a href='https://github.com/XXIIVV/webring'>Information</a> <a id='icon'  href='#random' onClick="portal.reload('random')"></a></p>`
+  }
   
   this.directory = function()
   {
@@ -22,7 +32,7 @@ function Portal(sites)
       var site = this.sites[id]
       html += `<ln>${id}) <a href='${site}'>${site.split("//")[1]}</a></ln>`
     }
-    return `<list>${html}</list>\n<a href='#random' onClick="portal.reload('random')">Random</a> | <a href='https://github.com/XXIIVV/webring'>Information</a> <a id='icon'  href='#random' onClick="portal.reload('random')"></a></ln>`
+    return `<list>${html}</list>\n${this.readme()}${this.buttons()}`
   }
 
   this.reload = function()
@@ -69,6 +79,7 @@ function Portal(sites)
     var location = this.locate();
     var target = this.next(location);
     this.navigate(target)
-    return `<p>Redirecting to <b>${target}</b></p><meta http-equiv="refresh" content="3; url=${target}"><a href='#' onClick="portal.reload('')">Directory</a> | <a href='#${target}' onClick="portal.reload('random')">Skip</a> | <a href='#random' onClick="portal.reload('random')">Random</a> | <a href='https://github.com/XXIIVV/webring'>Information</a> <a id='icon'  href='#random' onClick="portal.reload('random')"></a>`
+    return `<p>Redirecting to <b>${target}</b></p><meta http-equiv="refresh" content="3; url=${target}">
+    <p class='buttons'><a href='#' onClick="portal.reload('')">Directory</a> | <a href='#${target}' onClick="portal.reload('random')">Skip</a> | <a href='#random' onClick="portal.reload('random')">Random</a> | <a href='https://github.com/XXIIVV/webring'>Information</a> <a id='icon'  href='#random' onClick="portal.reload('random')"></a></p>`
   }
 }
