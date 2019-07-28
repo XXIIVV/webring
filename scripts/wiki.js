e9834eea2cf0cc52f2a4f1ef9a2bb626fb3fcccd
Devine Lu Linvega <aliceffekt@gmail.com>
2019-07-29 08:46:08 +0900

    Displaying categories in the sidebar

--- scripts/wiki.js
+++ scripts/wiki.js
@@ -3,20 +3,42 @@
 function Wiki (sites) {
   const feeds = {}
   this.sites = sites
-  this.el = document.createElement('div')
-  this.el.id = 'wiki'
+  this._el = document.createElement('div')
+  this._el.id = 'wiki'
+  this._sidebar = document.createElement('div')
+  this._sidebar.id = 'sidebar'
+  this._categories = document.createElement('ul')
+  this._categories.id = 'categories'
+  this._names = document.createElement('div')
+  this._names.id = 'names'
 
   this.index = {}
+  this.categories = {}
 
   this.install = (host) => {
     console.log('Wiki', 'Installing..')
     this.fetch()
-    host.appendChild(this.el)
+    this._el.appendChild(this._names)
+    this._sidebar.appendChild(this._categories)
+    this._el.appendChild(this._sidebar)
+    host.appendChild(this._el)
   }
 
   this.start = () => {
     console.log('Wiki', 'Starting..')
-    this.el.innerHTML = 'hello.'
+    // this.el.innerHTML = 'hello.'
+  }
+
+  this.refresh = () => {
+    console.log(Object.keys(this.index).length + ' terms')
+
+    // Cats
+
+    let html = ''
+    for (const id in this.categories) {
+      html += `<li>${id} <span class='right'>${this.categories[id].length}</span></li>`
+    }
+    this._categories.innerHTML = `${html}`
   }
 
   this.fetch = () => {
@@ -32,15 +54,15 @@ function Wiki (sites) {
     }
   }
 
-  this.refresh = () => {
-    console.log(Object.keys(this.index).length + ' terms')
-  }
-
   this.add = (name, value, cat, origin) => {
     if (!this.index[name]) {
       this.index[name] = []
     }
     this.index[name].push({ value, cat, origin })
+    if (!this.categories[cat]) {
+      this.categories[cat] = []
+    }
+    this.categories[cat].push({ value, cat, origin })
   }
 
   this.parse = (site, content) => {
