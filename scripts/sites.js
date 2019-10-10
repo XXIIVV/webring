dd2d2d8443188b73e805ca42fb97419810c623e4
Cristi Constantin <1748317+croqaz@users.noreply.github.com>
2019-10-10 22:19:30 +0000

    Add CrLf website to sites.js

--- scripts/sites.js
+++ scripts/sites.js
@@ -759,6 +759,14 @@ const sites = [
     title:  'raul altosaar',
     type: 'portfolio',
     url: 'https://www.raul.earth/'
-  }
+  },
 
+  {
+    author: 'Cristi Constantin',
+    url: 'https://crlf.site',
+    langs: ['en'],
+    rss: 'https://crlf.site/rss.xml',
+    title: 'Cr;Lf;',
+    type: 'hybrid'
+  }
 ]
