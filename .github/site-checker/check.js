3d521d5f05280da02d9fef612539505a174d1487
Travis Shears <t@travisshears.com>
2022-02-04 14:04:03 +0100

    refactor check.js

--- .github/site-checker/check.js
+++ .github/site-checker/check.js
@@ -1,89 +1,103 @@
-const fs = require('fs')
-const path = require('path');
-const { promisify } = require('util')
+const fs = require("fs/promises");
+const path = require("path");
 const jsdom = require("jsdom");
-const axios = require('axios').default;
+const axios = require("axios").default;
 
 const { JSDOM } = jsdom;
 
-function getIndexHTML() {
-    return new Promise((resolve, reject) => {
-        fs.readFile('../../index.html', 'utf8', function (err, data) {
-            if (err) {
-                return reject(err);
-            }
-            return resolve(data);
-        });
-    })
-}
+const readWebringHTMLFromFileSystem = () =>
+  fs.readFile("../../index.html", "utf8");
 
-async function getLinks(html) {
-    const { document } = (new JSDOM(html)).window;
-    const alllinks = Array.from(document.querySelectorAll('body > ol > li a'));
-    const siteLinks = alllinks.filter(link => link.classList.length === 0);
-    return siteLinks.map(link => link.getAttribute('href'));
-}
+const extractLinkFromWebringHTML = (html) => {
+  const { document } = new JSDOM(html).window;
+  const anchorElements = Array.from(
+    document.querySelectorAll("body > ol > li a")
+  );
+  return anchorElements
+    .map((link) => link.getAttribute("href"))
+    .filter((link) => link);
+};
 
-const validStatusCode = (code) => code >= 200 && code < 400;
+const validHTTPStatusCode = (code) => code >= 200 && code < 400;
 
-async function getStatusTotals(links) {
-    const total = links.length;
-    const errorMessages = [];
-    const getStatus = async (link) => {
-        try {
-            const res = await axios.get(link);
-            const valid = validStatusCode(res.status);
-            if (!valid) {
-                errorMessages.push(`${link} does not have a valid status code`);
-                return false;
-            }
-            return true;
-        } catch {
-            errorMessages.push(`${link} cound not be reached`);
-            return false;
-        }
+const validateLink = async (link) => {
+  const timeout = 30000; // 30 sec for the site to respond
+  const TIMEOUT_KEY = "TIMEOUT";
+  const timeoutFn = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(TIMEOUT_KEY);
+    }, timeout);
+  });
+  try {
+    const res = await Promise.race([timeoutFn, axios.get(link)]);
+    if (res === TIMEOUT_KEY) {
+      return {
+        link,
+        valid: false,
+        reason: `took longer then ${timeout / 1000} sec to respond`,
+      };
     }
-    const status = await Promise.all(links.map(getStatus));
-    const alive = status.filter(status => status).length;
+    const valid = validHTTPStatusCode(res.status);
+    if (valid) {
+      return { valid, link };
+    }
     return {
-        alive,
-        total,
-        errorMessages
-    }
-}
+      link,
+      valid,
+      reason: res.status,
+    };
+  } catch (e) {
+    return {
+      link,
+      valid: false,
+      reason: e,
+    };
+  }
+};
 
-async function getBadge(alive, total) {
-    const diff = total - alive;
-    let color = 'green';
-    if (diff > 0) {
-        color = 'yellow';
-    } 
-    if (diff > 5) {
-        color = 'red';
-    }
-    const res = await axios(`https://img.shields.io/badge/reachable%20sites-${alive}%2F${total}-${color}`);
-    return res.data;
-}
+const validateLinks = (links) => Promise.all(links.map(validateLink));
 
-// main
-(async function() {
-    const html = await getIndexHTML();
-    const links = await getLinks(html);
-    const {alive, total, errorMessages} = await getStatusTotals(links);
-    const badgeSvg = await getBadge(alive, total);
+const getBadge = async (alive, total) => {
+  const diff = total - alive;
+  let color = "green";
+  if (diff > 0) {
+    color = "yellow";
+  }
+  if (diff > 5) {
+    color = "red";
+  }
+  const res = await axios(
+    `https://img.shields.io/badge/reachable%20sites-${alive}%2F${total}-${color}`
+  );
+  return res.data;
+};
 
-    const badgePath = '../../_badges/reachable-site.svg';
-    const errorFilePath = '../../_badges/reachable-site-errors.txt';
-    const errorMessage = errorMessages.join('\r\n');
-    console.log('errorMessage', errorMessage);
-    const writeFileAsync = promisify(fs.writeFile);
-    const mkdirAsync = promisify(fs.mkdir);
-    await mkdirAsync(path.dirname(badgePath), { recursive: true });
-    await writeFileAsync(badgePath, badgeSvg);
-    await writeFileAsync(errorFilePath, errorMessage);
-    process.exit(0);
-})()
-.catch(e => {
-    console.log(`Error: ${e.message}`);
-    process.exit(1);
+const getErrorText = (linkStatus) => {
+  const unreachableStatus = linkStatus.filter((status) => !status.valid);
+  return unreachableStatus.reduce(
+    (carry, status) => carry + `${status.link}: ${status.reason}\r\n`,
+    ""
+  );
+};
+
+const badgePath = "../../_badges/reachable-site.svg";
+const errorFilePath = "../../_badges/reachable-site-errors.txt";
+
+(async function () {
+  const webringHTML = await readWebringHTMLFromFileSystem();
+  const links = extractLinkFromWebringHTML(webringHTML);
+  console.log(`found ${links.length} webring links`);
+  const linkStatus = await validateLinks(links);
+  const totalLinks = links.length;
+  const reachableLinks = linkStatus.filter((x) => x.valid).length;
+  console.log(`${reachableLinks} reachable sites out of ${totalLinks}`);
+  const badgeSvg = await getBadge(reachableLinks, totalLinks);
+
+  await fs.mkdir(path.dirname(badgePath), { recursive: true });
+  await fs.writeFile(badgePath, badgeSvg);
+  await fs.writeFile(errorFilePath, getErrorText(linkStatus));
+  process.exit(0);
+})().catch((e) => {
+  console.log(`Error: ${e.message}`);
+  process.exit(1);
 });
